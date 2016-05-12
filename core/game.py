0de98145c8d4ba5037ccd9e8e60ce26b04e9b62f
Jandro López <ajandro100@gmail.com>
2016-05-12 23:11:26 +0200

    Finish game logic

--- core/game.py
+++ core/game.py
@@ -1,4 +1,4 @@
-
+from collections import OrderedDict
 from random import shuffle, randrange
 import collections
 
@@ -15,12 +15,15 @@ class KniffelGame(object):
     FOUR_X = "four of a kind"
     FULL_HOUSE = "full house"
     SMALL_STRAIGHT = "small straight"
-    BIG_STRAIGHT = "big straight"
+    LARGE_STRAIGHT = "large straight"
     CHANCE = "chance"
     KNIFFEL = "kniffel"
+    UPPER_BONUS = "upper bonus"
+    KNIFFEL_BONUS = "kniffel bonus"
+
     possible_scores = [
         ONES, TWOS, THREES, FOURS, FIVES, SIXES,
-        THREE_X, FOUR_X, FULL_HOUSE, SMALL_STRAIGHT, BIG_STRAIGHT,
+        THREE_X, FOUR_X, FULL_HOUSE, SMALL_STRAIGHT, LARGE_STRAIGHT,
         CHANCE, KNIFFEL]
 
     def __init__(self):
@@ -28,18 +31,20 @@ class KniffelGame(object):
         self._players = []
         self._current_saved_die = None
         self._current_rolled_die = None
-        self._scores = dict()
+        self._scores = OrderedDict()
         self._current_player = None
+        self._roll_times = None
 
     def add_player(self, player):
         self._players.append(player)
-        self._scores[player] = {key: None for key in self.possible_scores}
+        self._scores[player] = OrderedDict([(key, None) for key in self.possible_scores + [self.UPPER_BONUS, self.KNIFFEL_BONUS]])
+        self._scores[player][self.KNIFFEL_BONUS] = 0
 
     def play(self):
         rounds = 13
         while rounds > 0:
             for player in self.players:
-                self._set_new_round()
+                self._set_new_round(player)
                 player.round(self)
 
         self._wind_up()
@@ -68,45 +73,87 @@ class KniffelGame(object):
 
     def score(self, category):
 
-        assert category in self._scores[self._current_player]
-        self._scores[self._current_player][category] = self.calculate_score(category)
+        scores = self._scores[self._current_player]
+
+        assert category in scores
+        assert scores[category] is None
+
+        previous_kniffel = scores[self.KNIFFEL] == 50
+        scores[category] = self.calculate_score(category)
+        kniffel = True if min(self.die) == max(self.die) else False
+
+        if previous_kniffel and kniffel:
+            # Extra points for second kniffel
+            scores[self.KNIFFEL_BONUS] += 100
+
+        # Reset
+        self._reset()
 
     def calculate_score(self, category):
 
         die = self.die
+        assert len(die) == 5
         kniffel = True if min(die) == max(die) else False
+        kniffel_dice = die[0] if kniffel else None
+
+        try:
+            joker = self._scores[self._current_player].values()[kniffel_dice - 1] is not None
+        except TypeError:
+            joker = False
 
         if category == self.ONES:
             unit_value = 1
-            return sum((value for value in die if value == unit_value)) if not kniffel else unit_value * 5
+            return sum((value for value in die if value == unit_value))
         elif category == self.TWOS:
             unit_value = 2
-            return sum((value for value in die if value == unit_value)) if not kniffel else unit_value * 5
+            return sum((value for value in die if value == unit_value))
         elif category == self.THREES:
             unit_value = 3
-            return sum((value for value in die if value == unit_value)) if not kniffel else unit_value * 5
+            return sum((value for value in die if value == unit_value))
         elif category == self.FOURS:
             unit_value = 4
-            return sum((value for value in die if value == unit_value)) if not kniffel else unit_value * 5
+            return sum((value for value in die if value == unit_value))
         elif category == self.FIVES:
             unit_value = 5
-            return sum((value for value in die if value == unit_value)) if not kniffel else unit_value * 5
+            return sum((value for value in die if value == unit_value))
         elif category == self.SIXES:
             unit_value = 6
-            return sum((value for value in die if value == unit_value)) if not kniffel else unit_value * 5
+            return sum((value for value in die if value == unit_value))
         elif category == self.THREE_X:
-            return 6*5 if kniffel else (
+            return 6*5 if joker else (
                 sum(die) if any(repetitions >= 3 for repetitions in collections.Counter(die).values()) else 0)
         elif category == self.FOUR_X:
-            return 6*5 if kniffel else (
+            return 6*5 if joker else (
                 sum(die) if any(repetitions >= 4 for repetitions in collections.Counter(die).values()) else 0)
+        elif category == self.FULL_HOUSE:
+            return 25 if joker or {3, 2}.issubset(collections.Counter(die).values()) else 0
+        elif category == self.SMALL_STRAIGHT:
+            return 30 if joker or any(option.issubset(die) for option in [{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}]) \
+                else 0
+        elif category == self.LARGE_STRAIGHT:
+            return 40 if joker or any(option.issubset(die) for option in [{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}]) else 0
+        elif category == self.CHANCE:
+            return sum(die)
+        elif category == self.KNIFFEL:
+            return 50 if kniffel else 0
 
         raise Exception
 
     @property
+    def scores(self):
+        return self._scores
+
+    @property
+    def my_score(self):
+        return sum(score for score in self._scores[self._current_player].values() if score is not None)
+
+    @property
     def die(self):
 
-        return self._current_rolled_die + (self._current_saved_die if self._current_saved_die is not None else [])
+        try:
+            return self._current_rolled_die + (self._current_saved_die if self._current_saved_die is not None else [])
+        except TypeError:
+            raise AssertionError
 
     @property
     def players(self):
@@ -116,10 +163,14 @@ class KniffelGame(object):
         shuffle(self._players)
 
     def _set_new_round(self, player):
+        self._reset()
+        self._current_player = player
+
+    def _reset(self):
+
         self._roll_times = 0
         self._current_saved_die = None
         self._current_rolled_die = None
-        self._current_player = player
 
     def _check_die_availability(self, die):
 
--- unittests/test_game.py
+++ unittests/test_game.py
@@ -1,5 +1,4 @@
-from mock.mock import PropertyMock
-
+from collections import OrderedDict
 from core.game import KniffelGame
 from unittest import TestCase
 from mock import patch
@@ -44,7 +43,7 @@ class TestKniffelGame(TestCase):
 
         game = KniffelGame()
 
-        self.assertRaises(AttributeError, getattr, game, '_roll_times')
+        self.assertIsNone(game._roll_times)
 
         game._set_new_round("dummy_player")
         self.assertEqual(game._roll_times, 0)
@@ -79,11 +78,13 @@ class TestKniffelGame(TestCase):
         self.assertEqual(1, len(rolled_die))
         self.assertEqual(rolled_die, game._current_rolled_die)
 
-        # Test implausible scenarios
+        # * Test implausible scenarios
+        # Assertion error because of no available die to be rolled
         game._set_new_round("dummy_player")
         game._current_saved_die = [1] * 5
         self.assertRaises(AssertionError, game.roll)
 
+        # Assertion error because of no available die to be rolled
         game._set_new_round("dummy_player")
         game._current_saved_die = [1] * 6
         self.assertRaises(AssertionError, game.roll)
@@ -107,7 +108,8 @@ class TestKniffelGame(TestCase):
 
         game._set_new_round("dummy_player")
 
-        self.assertRaises(TypeError, game._check_die_availability, [1])
+        # Test no rolled die
+        self.assertRaises(AssertionError, game._check_die_availability, [1])
 
         game._current_rolled_die = [1] * 3
         self.assertFalse(game._check_die_availability([6]))
@@ -141,7 +143,15 @@ class TestKniffelGame(TestCase):
         kniffel_of_fours = [4] * 5
         four = [1] * 4 + [5]
         poor = [1, 2, 3, 4, 5]
+        poorer = [1, 2, 3, 4, 6]
+        full_house = [2] * 2 + [3] * 3
+        all_upper_scores = {
+            "dummy_player": OrderedDict(
+                [(game.ONES, 1), (game.TWOS, 4), (game.THREES, 23), (game.FOURS, 2), (game.FIVES, 56), (game.SIXES, 0),
+                 (game.KNIFFEL, 0)])
+        }
 
+        # Upper section
         with patch.object(game, '_current_rolled_die', dummy_die):
             self.assertEqual(game.calculate_score(game.ONES), 1)
 
@@ -161,25 +171,86 @@ class TestKniffelGame(TestCase):
             self.assertEqual(game.calculate_score(game.SIXES), 0)
 
         with patch.object(game, '_current_rolled_die', kniffel_of_fours):
-            self.assertEqual(game.calculate_score(game.SIXES), 6*5)
+            self.assertEqual(game.calculate_score(game.SIXES), 0)
 
         with patch.object(game, '_current_rolled_die', dummy_die):
             self.assertEqual(game.calculate_score(game.THREE_X), sum(dummy_die))
 
+        # Three of a kind
         with patch.object(game, '_current_rolled_die', kniffel_of_fours):
-            self.assertEqual(game.calculate_score(game.THREE_X), 6*5)
+            self.assertEqual(game.calculate_score(game.THREE_X), 20)
+
+        with patch.object(game, '_current_rolled_die', kniffel_of_fours):
+            self.assertEqual(game.calculate_score(game.THREE_X), sum(kniffel_of_fours))
 
         with patch.object(game, '_current_rolled_die', poor):
             self.assertEqual(game.calculate_score(game.THREE_X), 0)
 
+        # Four of a kind
         with patch.object(game, '_current_rolled_die', four):
             self.assertEqual(game.calculate_score(game.FOUR_X), sum(four))
 
         with patch.object(game, '_current_rolled_die', kniffel_of_fours):
-            self.assertEqual(game.calculate_score(game.FOUR_X), 6*5)
+            self.assertEqual(game.calculate_score(game.FOUR_X), sum(kniffel_of_fours))
 
         with patch.object(game, '_current_rolled_die', poor):
             self.assertEqual(game.calculate_score(game.FOUR_X), 0)
+
+        # Full house
+        with patch.object(game, '_current_rolled_die', full_house):
+            self.assertEqual(game.calculate_score(game.FULL_HOUSE), 25)
+
+        with patch.object(game, '_current_rolled_die', full_house[3:] + full_house[:3]):
+            self.assertEqual(game.calculate_score(game.FULL_HOUSE), 25)
+
+        with patch.object(game, '_current_rolled_die', kniffel_of_fours), \
+             patch.object(game, '_scores', all_upper_scores):
+            self.assertEqual(game.calculate_score(game.FULL_HOUSE), 25)
+
+        with patch.object(game, '_current_rolled_die', poor):
+            self.assertEqual(game.calculate_score(game.FULL_HOUSE), 0)
+
+        # Small straight
+        with patch.object(game, '_current_rolled_die', poor):
+            self.assertEqual(game.calculate_score(game.SMALL_STRAIGHT), 30)
+
+        with patch.object(game, '_current_rolled_die', poorer):
+            self.assertEqual(game.calculate_score(game.SMALL_STRAIGHT), 30)
+
+            with patch.object(game, '_current_rolled_die', kniffel_of_fours):
+                self.assertEqual(game.calculate_score(game.SMALL_STRAIGHT), 0)
+
+        with patch.object(game, '_current_rolled_die', kniffel_of_fours), \
+             patch.object(game, '_scores', all_upper_scores):
+            self.assertEqual(game.calculate_score(game.SMALL_STRAIGHT), 30)
+
+        # Large straight
+        with patch.object(game, '_current_rolled_die', poor):
+            self.assertEqual(game.calculate_score(game.LARGE_STRAIGHT), 40)
+
+        with patch.object(game, '_current_rolled_die', poorer):
+            self.assertEqual(game.calculate_score(game.LARGE_STRAIGHT), 00)
+
+            with patch.object(game, '_current_rolled_die', kniffel_of_fours):
+                self.assertEqual(game.calculate_score(game.LARGE_STRAIGHT), 0)
+
+        with patch.object(game, '_current_rolled_die', kniffel_of_fours), \
+             patch.object(game, '_scores', all_upper_scores):
+            self.assertEqual(game.calculate_score(game.LARGE_STRAIGHT), 40)
+
+        # Chance
+        with patch.object(game, '_current_rolled_die', poor):
+            self.assertEqual(game.calculate_score(game.CHANCE), sum(poor))
+
+        with patch.object(game, '_current_rolled_die', kniffel_of_fours):
+            self.assertEqual(game.calculate_score(game.CHANCE), sum(kniffel_of_fours))
+
+        # Kniffel
+        with patch.object(game, '_current_rolled_die', poor):
+            self.assertEqual(game.calculate_score(game.KNIFFEL), 0)
+
+        with patch.object(game, '_current_rolled_die', kniffel_of_fours):
+            self.assertEqual(game.calculate_score(game.KNIFFEL), 50)
 
     def test_score(self):
 
@@ -195,3 +266,19 @@ class TestKniffelGame(TestCase):
 
         self.assertRaises(AssertionError, game.score, "SOME INVALID SCORE")
 
+        game.score(game.KNIFFEL)
+        self.assertEqual(game.my_score, 50)
+
+        # Cannot repeat score
+        self.assertRaises(AssertionError, game.score, game.KNIFFEL)
+
+        # Nothing rolled
+        self.assertRaises(AssertionError, game.score, game.ONES)
+
+        game._current_rolled_die = [1] * 4 + [3]
+        game.score(game.ONES)
+        self.assertEqual(game.my_score, 54)
+
+        game._current_rolled_die = [2] * 5
+        game.score(game.TWOS)
+        self.assertEqual(game.my_score, 54 + 100 + 2*5)
